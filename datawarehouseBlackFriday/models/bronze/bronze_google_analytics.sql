5b90cbf303cbbb8ecab8bc91beeae1aa674c2413
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-21 08:22:16 -0300

    changing data source in bronze folder

--- datawarehouseBlackFriday/models/bronze/bronze_google_analytics.sql
+++ datawarehouseBlackFriday/models/bronze/bronze_google_analytics.sql
@@ -1,7 +1,7 @@
 -- Required table CTE
 WITH google_analytics as (
     SELECT * 
-    FROM {{ source('datajourneydatapipeline_9nhn', 'google_analytics_zapflow')}}
+    FROM {{ source('della-postgre', 'google_analytics_zapflow')}}
 )
 
 
