1daf79d58df471e8a2a458a2013edc1ba039944b
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-21 08:32:07 -0300

    creating bronze_typerform.sql file

--- datawarehouseBlackFriday/models/bronze/bronze_typeform.sql
+++ datawarehouseBlackFriday/models/bronze/bronze_typeform.sql
@@ -6,11 +6,11 @@
 }
 
 -- Required table CTE
-WITH typeform_responses as (
+WITH typeform as (
     SELECT * 
     FROM {{ source('della-postgre', 'typeform_responses')}}
 )
 
 
 -- Creating View from my table CTE
-SELECT * FROM typeform_responses
+SELECT * FROM typeform
